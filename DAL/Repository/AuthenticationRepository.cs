9a2bcebff3eab59a393a3960e965595c85d046a5
Linkindias <Linkindias21617@gmail.com>
2019-08-07 16:40:48 +0800

    Refactory Dapper Query Method

--- DAL/Repository/AuthenticationRepository.cs
+++ DAL/Repository/AuthenticationRepository.cs
@@ -29,34 +29,32 @@ namespace DAL.Repository
             DynamicParameters parameters = new DynamicParameters();
             Authentication authentication = null;
 
-            using (var connection = new SqlConnection(this.ConnectionString))
+            if (EmployeeId > 0)
             {
-                if (EmployeeId > 0)
-                {
-                    sqlCmd += "WHERE EmployeesId = @EmpId";
-                    parameters.Add("@EmpId", EmployeeId);
-                }
-
-                if (!string.IsNullOrEmpty(CostomerId))
-                {
-                    sqlCmd += sqlCmd.IndexOf("WHERE") == -1 ? "Where " : "and ";
-                    sqlCmd += "CustomersId = @CusId";
-                    parameters.Add("@CusId", CostomerId);
-                }
-
-                if (!string.IsNullOrEmpty(PassWord))
-                {
-                    sqlCmd += " and Password = @PassWord";
-                    parameters.Add("@PassWord", PassWord);
-                }
-
-                sqlCmd += " and State = @State";
-                parameters.Add("@State", (int)DataStatus.Enable);
-
-                var result = this.GetFirstDefault<Authentication>(sqlCmd, parameters);
-                rtn = result.rtn;
-                authentication = result.result;
+                sqlCmd += "WHERE EmployeesId = @EmpId";
+                parameters.Add("@EmpId", EmployeeId);
             }
+
+            if (!string.IsNullOrEmpty(CostomerId))
+            {
+                sqlCmd += sqlCmd.IndexOf("WHERE") == -1 ? "Where " : "and ";
+                sqlCmd += "CustomersId = @CusId";
+                parameters.Add("@CusId", CostomerId);
+            }
+
+            if (!string.IsNullOrEmpty(PassWord))
+            {
+                sqlCmd += " and Password = @PassWord";
+                parameters.Add("@PassWord", PassWord);
+            }
+
+            sqlCmd += " and State = @State";
+            parameters.Add("@State", (int)DataStatus.Enable);
+
+            var result = this.GetFirstDefault<Authentication>(sqlCmd, parameters);
+            rtn = result.rtn;
+            authentication = result.result;
+
             return (rtn, authentication);
         }
     }
