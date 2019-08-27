ad974f6912993f5b549e49f7c374c8535dd547f1
Linkindias <Linkindias21617@gmail.com>
2019-08-27 17:18:02 +0800

    Adding  Authorize Claim

--- WebApplication1/Controllers/LogInController.cs
+++ WebApplication1/Controllers/LogInController.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BLL.InterFace;
 using DAL.DTOModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication1.Controllers
 {
+    [Authorize]
     public class LogInController : Controller
     {
         IAuthService AuthService;
@@ -23,14 +27,32 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public IActionResult Login(LogInDTO LogIn)
+        [AllowAnonymous]
+        public async Task<IActionResult> LoginAsync(LogInDTO LogIn)
         {
             var result = AuthService.LogIn(LogIn.account, LogIn.password);
             if (result.rtn.IsSuccess)
+            {
                 HttpContext.Session.SetString(LogIn.account, LogIn.account);
+                //創建一個身份認證
+                var claims = new List<Claim>() {
+                    new Claim(ClaimTypes.Sid, LogIn.account), //用戶ID
+                    new Claim(ClaimTypes.Name, result.guid.ToString())  //用戶名稱
+                    };
+
+                var identity = new ClaimsIdentity(claims, "Login");
+                var userPrincipal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync("LogInScheme", userPrincipal, new AuthenticationProperties
+                {
+                    ExpiresUtc = DateTime.UtcNow.AddMinutes(20),
+                    IsPersistent = false,
+                    AllowRefresh = false
+                });
+            }
             return View();
         }
 
+        
         public IActionResult Logout(string Id)
         {
             var result = AuthService.LogOut(Id);
