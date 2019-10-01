cfca4be1564e25030a411746bde00f3d41a300be
Linkindias <Linkindias21617@gmail.com>
2019-10-01 14:46:34 +0800

    Modify Profile BLL, Modify LogIn Password Style

--- BLL/Model/MemberService.cs
+++ BLL/Model/MemberService.cs
@@ -30,15 +30,42 @@ namespace BLL.Model
             int EmployeeId = 0;
             int.TryParse(Id, out EmployeeId);
 
-            MemberModel memberModel = new MemberModel();
+            MemberModel member = new MemberModel();
             var empResult = EmployeeRep.GetEmployeeById(EmployeeId, (int)DataStatus.Enable);
             var cusResult = CustomerRep.GetCustomerById(Id, (int)DataStatus.Enable);
 
-            if (empResult.rtn.IsSuccess && empResult.employee != null) memberModel.employee = empResult.employee;
+            //當有員工且無會員，則回傳員工資訊
+            if (empResult.employee != null && cusResult.customer == null)
+            {
+                member.employee = empResult.employee;
+                member.IsSuccess = true;
+                return member;
+            }
 
-            if (cusResult.rtn.IsSuccess && cusResult.customer != null) memberModel.customer = cusResult.customer;
+            //當有會員且無員工，則回傳會員資訊
+            if (cusResult.customer != null && empResult.employee == null)
+            {
+                member.customer = cusResult.customer;
+                member.IsSuccess = true;
+                return member;
+            }
 
-            return memberModel;
+            //當有取得員工有錯誤，則回傳取得員工錯誤訊息
+            if (!empResult.rtn.IsSuccess)
+            {
+                member.IsSuccess = empResult.rtn.IsSuccess;
+                member.ErrorMsg = empResult.rtn.ErrorMsg;
+                return member;
+            }
+
+            //當有取得會員有錯誤，則回傳取得會員錯誤訊息
+            if (!cusResult.rtn.IsSuccess)
+            {
+                member.IsSuccess = cusResult.rtn.IsSuccess;
+                member.ErrorMsg = cusResult.rtn.ErrorMsg;
+                return member;
+            }
+            return member;
         }
 
         /// <summary>
--- DAL/PageModel/MemberModel.cs
+++ DAL/PageModel/MemberModel.cs
@@ -1,11 +1,12 @@
-﻿using DAL.DBModel;
+﻿using Base;
+using DAL.DBModel;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DAL.PageModel
 {
-    public class MemberModel
+    public class MemberModel: Result
     {
         public Employees employee { get; set; }
         public Customers customer { get; set; }
